b17bbac771c7a72339c3e8efacbedff6639ee3d4
Moore <793665514@qq.com>
2020-07-24 17:36:50 +0800

    树型组件选择

--- src/components/system/User.vue
+++ src/components/system/User.vue
@@ -6,30 +6,35 @@
     </el-breadcrumb>
     <el-card class="card">
       <!-- 导航菜单 -->
-      <div>
+      <div style="width: 20%">
+        <!-- <template>
+          <a-directory-tree multiple default-expand-all @select="onSelect" @expand="onExpand">
+            <a-tree-node v-for="(item,arr) in treeList" :key="arr" :title="item.name">
+              <a-tree-node v-for="(itemSub,itemArr) in item.childMenu" :key="itemArr" :title="itemSub.name" is-leaf />
+            </a-tree-node>
+          </a-directory-tree>
+        </template> -->
         <template>
-          <a-tree-select
-            v-model="value"
-            show-search
-            style="width: 100%"
-            :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
-            placeholder="Please select"
-            allow-clear
-            tree-default-expand-all
-          >
-            <a-tree-select-node key="0-1" value="parent 1" title="parent 1">
-              <a-tree-select-node key="0-1-1" value="parent 1-0" title="parent 1-0">
-                <a-tree-select-node key="random" :selectable="false" value="leaf1" title="my leaf" />
-                <a-tree-select-node key="random1" value="leaf2" title="your leaf" />
-              </a-tree-select-node>
-              <a-tree-select-node key="random2" value="parent 1-1" title="parent 1-1">
-                <a-tree-select-node key="random3" value="sss">
-                  <b slot="title" style="color: #08c">sss</b>
-                </a-tree-select-node>
-              </a-tree-select-node>
-            </a-tree-select-node>
-          </a-tree-select>
-        </template>
+      <template>
+  <a-tree show-line :default-expanded-keys="['0-0-0']" @select="onSelect">
+    <a-icon slot="switcherIcon" type="down" />
+    <a-tree-node key="0-0" title="parent 1">
+      <a-tree-node key="0-0-0" title="parent 1-0">
+        <a-tree-node key="0-0-0-0" title="leaf" />
+        <a-tree-node key="0-0-0-1" title="leaf" />
+        <a-tree-node key="0-0-0-2" title="leaf" />
+      </a-tree-node>
+      <a-tree-node key="0-0-1" title="parent 1-1">
+        <a-tree-node key="0-0-1-0" title="leaf" />
+      </a-tree-node>
+      <a-tree-node key="0-0-2" title="parent 1-2">
+        <a-tree-node key="0-0-2-0" title="leaf" />
+        <a-tree-node key="0-0-2-1" title="leaf" />
+      </a-tree-node>
+    </a-tree-node>
+  </a-tree>
+</template>
+</template>
       </div>
       <!-- 结束 -->
     </el-card>
@@ -40,11 +45,42 @@
 export default {
   data() {
     return {
-      treeExpandedKeys: [],
-      value: undefined
+      treeList: [],
+      treeData: [
+        {
+          title: 'parent 1',
+          key: '0-0',
+          slots: {
+            icon: 'smile'
+          },
+          children: [
+            { title: 'leaf', key: '0-0-0', slots: { icon: 'iconfont icon-caidan' } },
+            { title: 'leaf', key: '0-0-1', scopedSlots: { icon: 'custom' } }
+          ]
+        }
+      ]
     }
+  },
+  methods: {
+    onSelect(selectedKeys, info) {
+      console.log('selected', selectedKeys, info);
+    },
+    onCheck(checkedKeys, info) {
+      console.log('onCheck', checkedKeys, info);
+    },
+    async getProductType() {
+      const { data: res } = await this.$http.get("product/type");
+      if (res.code !== 200) {
+        return this.$message.error("货品分类获取失败");
+      }
+      console.log(res.data)
+      // this.treeList = res.data
+    }
+  },
+  created() {
+    this.getProductType();
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
